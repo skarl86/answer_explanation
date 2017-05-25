2fe1a75cc2193e78a174df28977f8d5f6d830dba
NK <beohemian@gmail.com>
2017-05-25 18:14:03 +0900

    1. Reasoner 결과에 대한 예외처리 코드 추가 2. 출력 가독성 개선

--- src/main/scala/UnifyResult.scala
+++ src/main/scala/UnifyResult.scala
@@ -17,15 +17,16 @@ import scala.util.matching.Regex
 object UnifyResult {
 
   val XBP = "<http://xb.saltlux.com/schema/property/>"
+
   def main(args: Array[String]) {
     Logger.getLogger("org").setLevel(Level.WARN)
     Logger.getLogger("akka").setLevel(Level.WARN)
 
-//    System.setProperty("hadoop.home.dir", "C:\\hadoop-common-2.2.0-bin-master\\")
+    //    System.setProperty("hadoop.home.dir", "C:\\hadoop-common-2.2.0-bin-master\\")
 
     val conf = new SparkConf()
       .setAppName("Unify Result")
-      .setMaster("local[*]")
+      //      .setMaster("local[*]")
       .set("spark.io.compression.codec", "lz4")
       .set("spark.broadcast.compress", "true")
       .set("spark.locality.wait", "10000")
@@ -38,42 +39,66 @@ object UnifyResult {
     val queryPath = args(1)
     val triplePath = args(2)
     val unifiedResultPath = args(3)
-//    val resultPath = "query_result"
-//    val queryPath = "MJ-TestQuery/TQ1.rq"
-//    val triplePath = "nk,NK-TEST"
-//    val unifiedResultPath = "unified_result"
+    //    val resultPath = "query_result"
+    //    val queryPath = "MJ-TestQuery/TQ4.rq"
+    //    val triplePath = "nk,NK-TEST"
+    //    val unifiedResultPath = "unified_result"
     val queryFile = Source.fromFile(queryPath)
     val queryString = Source.fromFile(queryPath).getLines().mkString(" ")
-    val (variable, condition) =  parseCondition(queryString)
-    val resultsList= sc.textFile(resultPath).map(_.replace("(","").replace(")","").split(",").toList).collect().toList
+    val (variable, condition) = parseCondition(queryString)
+    val resultsList = sc.textFile(resultPath).map(_.replace("(", "").replace(")", "").split(",").toList).filter(exceptionHandling1).collect().toList
     val triple = sc.textFile(triplePath).mapPartitions(parseNTriple, true).collect().toList
 
-    println("=========== Query ===========")
+    printProgressTitle("=========== Query ===========")
     queryFile.getLines().foreach(println)
 
-    println("=========== Query Result ===========")
+    printProgressTitle("=========== Query Result ===========")
     resultsList.foreach(println)
 
-    println("=========== Query Variable ===========")
+    printProgressTitle("=========== Query Variable ===========")
     variable.foreach(println)
-    println("=========== Query Condition ===========")
+    printProgressTitle("=========== Query Condition ===========")
     condition.foreach(println)
 
-    val unifiedResultList:List[String] = unifyResult(condition, variable, resultsList)
-    println("=========== Unified Triple ===========")
+    val unifiedResultList: List[String] = unifyResult(condition, variable, resultsList)
+    printProgressTitle("=========== Unified Triple ===========")
     unifiedResultList.foreach(println)
-    println("=========== Filtering Triple ===========")
-    val filteredTriple = filtering(unifiedResultList, triple).map{case (s, p, o) => List(s, p, o).mkString("")}
+    printProgressTitle("=========== Filtering Triple ===========")
+    val filteredTriple = filtering(unifiedResultList, triple).filter(exceptionHandling2).map { case (s, p, o) => List(s, p, o).mkString("") }
     filteredTriple.foreach(println)
     sc.parallelize(filteredTriple).coalesce(1).saveAsTextFile(unifiedResultPath)
-    println("=========== ATMS Environment===========")
+    printProgressTitle("=========== Answer Explanation ===========")
     atms(sc, unifiedResultPath)
   }
-  def parseCondition(queryString:String):(List[String], List[String]) = {
+
+  def exceptionHandling1(resultList: List[String]): Boolean = {
+    /**
+      * reasoner를 통해 inferred 된 트리플 중
+      * person1 spouse person1
+      * 위와 같은 경우에 대한 예외처리.
+      */
+    if (resultList.size == 2) {
+      !(resultList(0) == resultList(1))
+    } else {
+      true
+    }
+  }
+
+  def exceptionHandling2(filteredTripleList: (String, String, String)): Boolean = {
+    /**
+      * reasoner를 통해 inferred 된 트리플 중
+      * unify된 결과 중
+      * person1 sibling person1
+      * 과 같이 subject와 object가 같은 triple을 예외처리하기 위한 코드.
+      */
+    !(filteredTripleList._1 == filteredTripleList._3)
+  }
+
+  def parseCondition(queryString: String): (List[String], List[String]) = {
     val newQueryString = queryString.replace("\t", "")
     val reg = new Regex("SELECT (\\?.+) WHERE (\\{.+\\})")
     val variable = reg.findAllIn(newQueryString).matchData.next().group(1).split("\\,").map(_.trim).toList
-    val condition = reg.findAllIn(newQueryString).matchData.next().group(2).replace("{", "").replace("}", "").replace("     ", "").trim.split(" \\.").toList
+    val condition = reg.findAllIn(newQueryString).matchData.next().group(2).replace("{", "").replace("}", "").replace("     ", "").replace("\t", "").trim.split(" \\.").map(_.trim).toList
 
     (variable, condition)
   }
@@ -84,26 +109,25 @@ object UnifyResult {
       "}" -> "",
       "     " -> ""
     )
-
-    substitutions.foldLeft(str) { case (cur, (from, to)) => cur.replaceAll(from, to)}
+    substitutions.foldLeft(str) { case (cur, (from, to)) => cur.replaceAll(from, to) }
   }
 
-  def filtering(unifiedResult: List[String], triple:List[(String, String, String)]): List[(String, String, String)] = {
+  def filtering(unifiedResult: List[String], triple: List[(String, String, String)]): List[(String, String, String)] = {
     val filteredTriple = scala.collection.mutable.ArrayBuffer.empty[List[(String, String, String)]]
 
-    for(result <- unifiedResult){
+    for (result <- unifiedResult) {
       val us = result.split(" ")(0)
       val up = result.split(" ")(1).split(":")(1)
       val uo = result.split(" ")(2)
 
       var mask = false
 
-      filteredTriple += triple.filter{case (s, p, o) =>
-        if(us.contains("?")){
+      filteredTriple += triple.filter { case (s, p, o) =>
+        if (us.contains("?")) {
           mask = p.contains(up) && o.contains(uo)
-        }else if(uo.contains("?")){
+        } else if (uo.contains("?")) {
           mask = s.contains(us) && p.contains(up)
-        }else{
+        } else {
           mask = s.contains(us) && p.contains(up) && o.contains(uo)
         }
         mask
@@ -112,13 +136,13 @@ object UnifyResult {
     filteredTriple.toList.flatten
   }
 
-  def unifyResult(conditionList:List[String], variables:List[String], resultList:List[List[String]]) = {
+  def unifyResult(conditionList: List[String], variables: List[String], resultList: List[List[String]]) = {
     val unifiedTriple = scala.collection.mutable.ArrayBuffer.empty[String]
 
-    for(result <- resultList){
-      for(condition <- conditionList){
+    for (result <- resultList) {
+      for (condition <- conditionList) {
         var temp = condition
-        for(i <- 0 to variables.length - 1){
+        for (i <- 0 to variables.length - 1) {
           temp = temp.replace(variables(i), result(i))
         }
         unifiedTriple += temp
@@ -130,12 +154,12 @@ object UnifyResult {
   def parseNTriple(lines: Iterator[String]) = {
     val TripleParser = new Regex("(<[^\\s]*>)|(_:[^\\s]*)|(\".*\")")
     for (line <- lines) yield {
-      try{
+      try {
         val tokens = TripleParser.findAllIn(line)
         val (s, p, o) = (tokens.next(), tokens.next(), tokens.next())
         (s, p, o)
 
-      }catch {
+      } catch {
         case nse: NoSuchElementException => {
           ("ERROR", "ERROR", "ERROR")
         }
@@ -144,7 +168,7 @@ object UnifyResult {
 
   }
 
-  def atms(sc:SparkContext, unifiedResultPath:String): Unit ={
+  def atms(sc: SparkContext, unifiedResultPath: String): Unit = {
 
     var indexedHolds: IndexedRDD[String, Set[Set[Int]]] = null
     var indexedAssumptions: IndexedRDD[Long, String] = null
@@ -157,22 +181,22 @@ object UnifyResult {
     var indexedJustifieds: IndexedRDD[String, Iterable[Long]] = null
     var nogoodListInt: Iterable[List[Int]] = Iterable.empty
 
-    indexedHolds = IndexedRDD( sc.objectFile("output/holds") )
+    indexedHolds = IndexedRDD(sc.objectFile("output/holds"))
     indexedHolds.count()
-    indexedAssumptions = IndexedRDD( sc.objectFile("output/assumptions") )
+    indexedAssumptions = IndexedRDD(sc.objectFile("output/assumptions"))
     indexedAssumptions.count()
 
-    indexedJustificands = IndexedRDD( sc.objectFile("output/justs") )
+    indexedJustificands = IndexedRDD(sc.objectFile("output/justs"))
     indexedJustificands.count()
-    indexedEnvs = IndexedRDD( sc.objectFile("output/envs") )
+    indexedEnvs = IndexedRDD(sc.objectFile("output/envs"))
     indexedEnvs.count()
-    indexedJustifiers = IndexedRDD( sc.objectFile("output/justifiers") )
+    indexedJustifiers = IndexedRDD(sc.objectFile("output/justifiers"))
     indexedJustifiers.count()
-    indexedJustifieds = IndexedRDD( sc.objectFile("output/justifieds"))
-    indexedNodes = IndexedRDD( sc.objectFile("output/indexedNodes"))
+    indexedJustifieds = IndexedRDD(sc.objectFile("output/justifieds"))
+    indexedNodes = IndexedRDD(sc.objectFile("output/indexedNodes"))
 
     val unifiedResultList = sc.textFile(unifiedResultPath).collect().toList
-    for(unifiedResult <- unifiedResultList){
+    for (unifiedResult <- unifiedResultList) {
       val nodename = unifiedResult
       val time0 = System.currentTimeMillis()
 
@@ -186,41 +210,56 @@ object UnifyResult {
         // assumId, assumTriple
         val assumpTriples: Map[Int, String] =
           indexedAssumptions.multiget(assumpOfEnv)
-            .map { case (assumId, assumTriple) => (assumId.toInt, assumTriple)}
+            .map { case (assumId, assumTriple) => (assumId.toInt, assumTriple) }
 
 
         //println("-------------- Environments of Label ----------------")
 
         var count = 0
-        println("Query Result :")
-        println(nodename)
-        if(envs.size != 1){
-          for (env <- envs) {
-            //          println("Env" + localEnvIndices.get(env.mkString(",")).getOrElse(0))
+        printExp1("Query Result [" + unifiedResultList.indexOf(unifiedResult) +"] :")
+        printExp3(nodename)
+        for (env <- envs) {
+          if (env.size != 1) {
             for (a <- env) {
               val triple = assumpTriples.get(a).get // URIs
-              val output = "   " + triple
+              val output = "--" + triple
               println(output)
             }
-            println()
             count += 1
           }
-        }else{
           println()
         }
-
-//        println("env count : " + count)
-
       } catch {
         case e: Exception => {
-          println("Query Result :")
-          println(nodename)
+          printExp1("Query Result [" + unifiedResultList.indexOf(unifiedResult) +"] :")
+//          e.printStackTrace()
+          printExp3(nodename)
           println()
         }
       }
-//      println("runtime : " + (System.currentTimeMillis() - time0) / 1000.0 + " sec")
+      //      println("runtime : " + (System.currentTimeMillis() - time0) / 1000.0 + " sec")
     }
 
+  }
+
+  def explanation(): Unit = {
+
+  }
+
+  def printProgressTitle(str: String) = {
+    println(Console.BOLD + Console.GREEN + str + Console.RESET)
+  }
+
+  def printExp1(str: String) = {
+    println(Console.YELLOW + str + Console.RESET)
+  }
+
+  def printExp2(str: String) = {
+    println(Console.MAGENTA + str + Console.RESET)
+  }
+
+  def printExp3(str: String) = {
+    println(Console.CYAN + str + Console.RESET)
   }
 
 }
